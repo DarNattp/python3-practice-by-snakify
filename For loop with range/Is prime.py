46630cee9a5f99ef526f43ba0a4f836959001b38
DarNattp <DarNattp@github.com>
2020-04-06 15:42:31 +0700

    last

--- For loop with range/Is prime.py
+++ For loop with range/Is prime.py
@@ -1,6 +1,6 @@
-''''A prime number is a natural number greater than 1 that has no 
+'''A prime number is a natural number greater than 1 that has no 
 positive divisors other than 1 and itself. Given an integer N > 1, print PRIME if it's prime and print COMPOSITE otherwise.
-''''
+'''
 
 a = int(input())
 
