5769c7a9d1f6818ea782e13181f2d5246a66e291
DarNattp <DarNattp@github.com>
2020-04-16 01:14:27 +0700

    first

--- if-else/sigle num.py
+++ if-else/sigle num.py
@@ -1,12 +1,19 @@
-print(5^2)
-n = 71
-first = 0
-sec = 0
-'''while n != 1 or n != 4:
+n = 2
+while n != 1 and n!= 4:
+    if len(str(n)) == 2 or len(str(n)) == 1:
     
-    first = n // 10
-    sec = n % 10
+        
+        first = n // 10
+        sec = n % 10
 
-    n = first**2 + sec**2
-'''
+        n = first**2 + sec**2
+    elif len(str(n)) == 3:
+    
+
+        first = n // 100
+        mid = n%100 // 10
+        sec = n%10
+
+        n = first**2 + mid**2 + sec**2
+
 print(n)
